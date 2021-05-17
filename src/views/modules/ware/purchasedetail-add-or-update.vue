fe442dd340558846e5437c4d8025d300accb2583
yanh <891367701@qq.com>
2021-05-17 13:40:23 +0800

    up

--- src/views/modules/ware/purchasedetail-add-or-update.vue
+++ src/views/modules/ware/purchasedetail-add-or-update.vue
@@ -11,23 +11,27 @@
       @keyup.enter.native="dataFormSubmit()"
       label-width="120px"
     >
-      <el-form-item label="sku_id" prop="skuId">
-        <el-input v-model="dataForm.skuId" placeholder="sku_id"></el-input>
+      <el-form-item label="采购商品id" prop="skuId">
+        <el-input v-model="dataForm.skuId" placeholder="采购商品id"></el-input>
+      </el-form-item>
+      <el-form-item label="采购数量" prop="skuNum">
+        <el-input v-model="dataForm.skuNum" placeholder="采购数量"></el-input>
       </el-form-item>
       <el-form-item label="仓库" prop="wareId">
         <el-select v-model="dataForm.wareId" placeholder="请选择仓库" clearable>
           <el-option :label="w.name" :value="w.id" v-for="w in wareList" :key="w.id"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="库存数" prop="stock">
-        <el-input v-model="dataForm.stock" placeholder="库存数"></el-input>
-      </el-form-item>
-      <el-form-item label="sku_name" prop="skuName">
-        <el-input v-model="dataForm.skuName" placeholder="sku_name"></el-input>
-      </el-form-item>
-      <el-form-item label="锁定库存" prop="stockLocked">
-        <el-input v-model="dataForm.stockLocked" placeholder="锁定库存"></el-input>
-      </el-form-item>
+      <!-- [0新建，1已分配，2正在采购，3已完成，4采购失败] -->
+      <!-- <el-form-item label="状态" prop="status">
+        <el-select v-model="dataForm.status" placeholder="请选择状态" clearable>
+          <el-option label="新建" :value="0"></el-option>
+          <el-option label="已分配" :value="1"></el-option>
+          <el-option label="正在采购" :value="2"></el-option>
+          <el-option label="已完成" :value="3"></el-option>
+          <el-option label="采购失败" :value="4"></el-option>
+        </el-select>
+      </el-form-item>-->
     </el-form>
     <span slot="footer" class="dialog-footer">
       <el-button @click="visible = false">取消</el-button>
@@ -44,21 +48,21 @@ export default {
       wareList: [],
       dataForm: {
         id: 0,
+        purchaseId: "",
         skuId: "",
+        skuNum: "",
+        skuPrice: "",
         wareId: "",
-        stock: 0,
-        skuName: "",
-        stockLocked: 0
+        status: 0
       },
       dataRule: {
-        skuId: [{ required: true, message: "sku_id不能为空", trigger: "blur" }],
-        wareId: [
-          { required: true, message: "仓库id不能为空", trigger: "blur" }
+        skuId: [
+          { required: true, message: "采购商品id不能为空", trigger: "blur" }
         ],
-        stock: [{ required: true, message: "库存数不能为空", trigger: "blur" }],
-        skuName: [
-          { required: true, message: "sku_name不能为空", trigger: "blur" }
-        ]
+        skuNum: [
+          { required: true, message: "采购数量不能为空", trigger: "blur" }
+        ],
+        wareId: [{ required: true, message: "仓库id不能为空", trigger: "blur" }]
       }
     };
   },
@@ -85,16 +89,19 @@ export default {
         this.$refs["dataForm"].resetFields();
         if (this.dataForm.id) {
           this.$http({
-            url: this.$http.adornUrl(`/ware/waresku/info/${this.dataForm.id}`),
+            url: this.$http.adornUrl(
+              `/ware/purchasedetail/info/${this.dataForm.id}`
+            ),
             method: "get",
             params: this.$http.adornParams()
           }).then(({ data }) => {
             if (data && data.code === 0) {
-              this.dataForm.skuId = data.wareSku.skuId;
-              this.dataForm.wareId = data.wareSku.wareId;
-              this.dataForm.stock = data.wareSku.stock;
-              this.dataForm.skuName = data.wareSku.skuName;
-              this.dataForm.stockLocked = data.wareSku.stockLocked;
+              this.dataForm.purchaseId = data.purchaseDetail.purchaseId;
+              this.dataForm.skuId = data.purchaseDetail.skuId;
+              this.dataForm.skuNum = data.purchaseDetail.skuNum;
+              this.dataForm.skuPrice = data.purchaseDetail.skuPrice;
+              this.dataForm.wareId = data.purchaseDetail.wareId;
+              this.dataForm.status = data.purchaseDetail.status;
             }
           });
         }
@@ -106,16 +113,17 @@ export default {
         if (valid) {
           this.$http({
             url: this.$http.adornUrl(
-              `/ware/waresku/${!this.dataForm.id ? "save" : "update"}`
+              `/ware/purchasedetail/${!this.dataForm.id ? "save" : "update"}`
             ),
             method: "post",
             data: this.$http.adornData({
               id: this.dataForm.id || undefined,
+              purchaseId: this.dataForm.purchaseId,
               skuId: this.dataForm.skuId,
+              skuNum: this.dataForm.skuNum,
+              skuPrice: this.dataForm.skuPrice,
               wareId: this.dataForm.wareId,
-              stock: this.dataForm.stock,
-              skuName: this.dataForm.skuName,
-              stockLocked: this.dataForm.stockLocked
+              status: this.dataForm.status
             })
           }).then(({ data }) => {
             if (data && data.code === 0) {
